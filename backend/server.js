1d04beae2d2ac5d5cb49985319d35d1bea2818e2
sofia-iliana <sofia.iliana1311@gmail.com>
2023-11-18 23:06:13 +0200

    fixing cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.json({ limit: "50mb", extended: true }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "https://medical-records-minp.onrender.com" }));
 
 app.use("/", userRouter);
 app.use("/", doctorRouter);
